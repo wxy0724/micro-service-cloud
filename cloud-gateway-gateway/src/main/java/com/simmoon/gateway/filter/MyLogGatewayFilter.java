d987c8234250db8ef101a1dba1f48f3c0dad3f83
wxy0724 <bryan0724@126.com>
2020-10-18 23:53:36 +0800

    init spring cloud gateway

---
+++ cloud-gateway-gateway/src/main/java/com/simmoon/gateway/filter/MyLogGatewayFilter.java
@@ -0,0 +1,34 @@
+package com.simmoon.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@Component
+@Slf4j
+public class MyLogGatewayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("***************come in MyLogGatewayFilter" + LocalDateTime.now());
+        String username = exchange.getRequest().getQueryParams().getFirst("username");
+        if (username == null) {
+            log.info("username不能为null");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
